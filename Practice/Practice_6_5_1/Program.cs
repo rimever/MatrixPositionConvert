1eccc32633dde9ace7df9bba865478a6644e60ca
rimever <mot_siva_ordin@hotmail.com>
2021-01-19 21:00:21 +0900

    Convert Position using Matrix

---
+++ Practice/Practice_6_5_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Practice_6_5_1
+{
+    /// <summary>
+    /// 書籍「ゲーム開発者のための物理・数学入門」より
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                @"２次元の物体を一度のステップでその中心点周りに-90度回転させるための組み合わせ行列を作りなさい。
+ただし、物体の中心点の座標は(10,50)とします。");
+            var move = Matrix.Build.DenseOfArray(new[,]
+            {
+                {1d, 0d, 10d},
+                {0d, 1d, 50d},
+                {0d, 0d, 1d}
+            });
+            var radian = -90d * Math.PI / 180d;
+            var rotate =Matrix.Build.DenseOfArray(new[,]
+            {
+                {Math.Cos(radian), -Math.Sin(radian), 0d},
+                {Math.Sin(radian), Math.Cos(radian), 0d},
+                {0d, 0d, 1d}
+            });
+            var move2 = Matrix.Build.DenseOfArray(new[,]
+            {
+                {1d, 0d, -10d},
+                {0d, 1d, -50d},
+                {0d, 0d, 1d}
+            });
+            var mergeMatrix = move * rotate * move2;
+            Console.WriteLine(mergeMatrix);
+            Console.WriteLine(@"作成した行列を使って、頂点がA(-10,50),B(90,0),C(-50,100)の三角形を回転させなさい");
+            var a = Matrix.Build.DenseOfArray(new[,]
+            {
+                {-10d},
+                {50d},
+                {1d}
+            });
+            Console.WriteLine(mergeMatrix * a);
+            var b = Matrix.Build.DenseOfArray(new[,]
+            {
+                {90d},
+                {0d},
+                {1d}
+            });
+            Console.WriteLine(mergeMatrix * b);
+            var c = Matrix.Build.DenseOfArray(new[,]
+            {
+                {-50d},
+                {100d},
+                {1d}
+            });
+            Console.WriteLine(mergeMatrix * c);
+        }
+    }
+}
---
+++ Practice/Practice_6_5_5/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Practice_6_5_5
+{
+    /// <summary>
+    /// 書籍「ゲーム開発者のための物理・数学入門」より
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                @"３次元の物体を45度ロール、90度ピッチ、-90度ヨーさせるための行列の一般式を一つの組み合わせ行列を使って作成しなさい。");
+            var radian = 45d * Math.PI / 180d;
+            var roll = Matrix.Build.DenseOfArray(new[,]
+            {
+                {Math.Cos(radian), -Math.Sin(radian), 0d, 0d},
+                {Math.Sin(radian), Math.Cos(radian), 0d, 0d},
+                {0d, 0d, 1d, 0d},
+                {0d, 0d, 0d, 1d}
+            });
+            radian = 90d * Math.PI / 180d;
+            var pitch = Matrix.Build.DenseOfArray(new[,]
+            {
+                {1d, 0d, 0d, 0d},
+                {0d, Math.Cos(radian), -Math.Sin(radian), 0d},
+                {0d, Math.Sin(radian), Math.Cos(radian), 0d},
+                {0d, 0d, 0d, 1d}
+            });
+            radian = -90d * Math.PI / 180d;
+            var yaw = Matrix.Build.DenseOfArray(new[,]
+            {
+                {Math.Cos(radian), 0d, Math.Sin(radian), 0d},
+                {0d, 1d, 0d, 0d},
+                {-Math.Sin(radian), 0d, Math.Cos(radian), 0d},
+                {0d, 0d, 0d, 1d}
+            });
+            // 発生する順に右から並べる
+            var mergeMatrix = yaw * pitch * roll;
+            Console.WriteLine(mergeMatrix);
+            Console.WriteLine("また、その式を使って頂点がD(200,0,-30),E(0,50,-150), F(40,20,-100)の三角形を回転させなさい。");
+            Console.WriteLine(mergeMatrix * Matrix.Build.DenseOfArray(new[,]
+            {
+                {200d},
+                {0d},
+                {-30d},
+                {1d}
+            }));
+            Console.WriteLine(mergeMatrix * Matrix.Build.DenseOfArray(new[,]
+            {
+                {0d},
+                {50d},
+                {-150d},
+                {1d}
+            }));
+            Console.WriteLine(mergeMatrix * Matrix.Build.DenseOfArray(new[,]
+            {
+                {40d},
+                {20d},
+                {-100d},
+                {1d}
+            }));
+            Console.WriteLine("DirectX（行ベクトル）で使える形にしなさい");
+            var convertDirectX = mergeMatrix.Inverse(); 
+            Console.WriteLine(convertDirectX);
+            Console.WriteLine(Matrix.Build.DenseOfArray(new[,]
+            {
+                {200d,0d,-30d,1d}
+            }) * convertDirectX);
+            Console.WriteLine(Matrix.Build.DenseOfArray(new[,]
+            {
+                {0d,50d,-150d,1d}
+            }) * convertDirectX);
+            Console.WriteLine(Matrix.Build.DenseOfArray(new[,]
+            {
+                {40d,20d,-100d,1d}
+            }) * convertDirectX);
+        }
+    }
+}
